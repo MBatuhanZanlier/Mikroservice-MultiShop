83603e8f7c9e2d9ecab1ee92103afbf9f5ce7b66
Batuhan Zanlıer <mucahitbatuhan06@gmail.com>
2025-01-20 10:14:19 +0300

    Add project files.

---
+++ Services/Discount/MultiShop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Discount.Dtos;
+
+namespace MultiShop.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultCouponDto>> GetAllDiscountCouponAsync();
+        Task CreateDiscountCouponAsync(CreateCouponDto createCouponDto);
+        Task UpdateDiscountCouponAsync(UpdateCouponDto updateCouponDto);
+        Task DeleteDiscountCouponAsync(int id);
+        Task<GetByIdCouponDto> GetByIdDiscountCouponAsync(int id);
+        Task<ResultCouponDto> GetCodeDetailByCodeAsync(string code);
+        int GetDiscountCouponCountRate(string code);
+        Task<int> GetDiscountCouponCount();
+    }
+}
